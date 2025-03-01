6682b8da20235ec1170903cb43fd00b86aae1028
blaze <blazespiritzzzz@gmail.com>
2025-03-01 22:47:41 +0800

    Implement date time formatter in utils.

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -3,3 +3,22 @@ export const assertUnreachable = (value: never): never => {
     `Statement should be unreachable. Unexpected value: ${value}}`
   );
 };
+
+export const formatDateTime = (date: Date): string => {
+  const dayOfMonth = date.getDate();
+  const month = date.getMonth();
+  const year = date.getFullYear();
+
+  const hour = date.getHours();
+  const minute = date.getMinutes();
+  const ampm = hour > 12 ? "pm" : "am";
+  const hour12 = hour > 12 ? hour - 12 : hour;
+
+  // pad 0 if less than 2 digit
+  const dayOfMonthStr = `${dayOfMonth}`.padStart(2, "0");
+  const monthStr = `${month}`.padStart(2, "0");
+  const hourStr = `${hour12}`.padStart(2, "0");
+  const minuteStr = `${minute}`.padStart(2, "0");
+
+  return `${dayOfMonthStr}-${monthStr}-${year} ${hourStr}:${minuteStr}${ampm}`;
+};
